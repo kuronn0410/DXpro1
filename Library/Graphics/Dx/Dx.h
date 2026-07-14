280ac5f9742cac15002c79820acc2f7359170bce
kuronn0410 <yamayuu7745@gmail.com>
2026-07-15 00:22:14 +0900

    ファイル訂正

--- Library/Graphics/Dx/Dx.h
+++ Library/Graphics/Dx/Dx.h
@@ -1,7 +1,12 @@
 #pragma once
+#include <d3d9.h>
 class Dx
 {
 public:
 	
 private:
+	IDirect3D9* g_direct3D = nullptr;// Direct3D9オブジェクトのポインタを保持する
+	IDirect3DDevice9* g_device = nullptr;// Direct3D9デバイスのポインタを保持する
+	D3DPRESENT_PARAMETERS g_params = {};
+
 };
